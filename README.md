9b81a5b3dd13fef10d33ef868d5af01cd09e89e5
Carl Stephen Saul <carlstephensaul@gmail.com>
2025-10-06 22:47:46 -0500

    Working fibd with num crate. Built off fib.

--- README.md
+++ README.md
@@ -1,42 +1,16 @@
-# fib
+# fibd
 
 Count of bnuuies total following the Finobacci sequence.
+One pair of offspring (k) per pair.
 - Solution for Rosalind's [Rabbits and Recurrence Relations](https://rosalind.info/problems/fib/).
 
-Uses clap.rs.
+Uses clap.rs and num.
 
 # Usage
 
 ```
-fib <N> <K>
+fibd <N> <m>
 ```
 
-Where...
-- N: number of months or length of Fibonacci sequence.
-- K: number of bnuuy pairs in litter; or a coefficient applied to the addend in Finobacci Seqeuence.
-
-## Examples
-
-### Output to file (sh)
-Works in Linux terminals.
-```
-$ cargo run -- $(cat test.txt) > test_output.txt
-    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.01s
-     Running `target/debug/fib 32 5`
-```
-
-### Output to Terminal
-To directly input and receive values, feel free to simply input them as separate args.
-```
-$ cargo run -- 5 3
-    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.01s
-     Running `target/debug/fib 5 3`
-19
-```
-
-# Updates and Comments
-
-## [2025-09-28]
-I was considering using a vector of strings and indexing--though that would push Rust to use the heap;
-it would also mean cloning the String on the heap when doubling the capacity at powers of two.
-- Bnuuy
+- N: Total duration of experiment in months
+- M: Lifetime of bnuuies in months
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,58 @@
 use clap::Parser;
-const ABOUT_MESSAGE: &str = "Count of bnuuies total following the Finobacci sequence.";
+use num::bigint::BigUint;
+use std::collections::VecDeque;
+
+const ABOUT_MESSAGE: &str = "Count of bnuuies total following the Finobacci sequence.
+One pair of offspring (k) per pair.";
 
 #[derive(Parser)]
 #[command(version, about = ABOUT_MESSAGE, long_about = None)]
 struct Cli {
-    /// Months
+    /// Total duration of experiment in months
     n: usize,
 
-    /// Number of bnuuy pairs per litter per month
-    k: usize,
+    /// Lifetime of bnuuies in months
+    m: usize,
 }
 
-pub fn bnuuy_pair_count(n: usize, k: usize) -> usize {
-    let mut count = 1;
-
+fn bnuuy_pair_count(n: usize, m: usize) -> BigUint {
+    let zero = || BigUint::ZERO;
+    let one = || BigUint::try_from(1).unwrap();
     //no new bnuuies until after 2nd month
     if n <= 2 {
-        return count;
+        return one();
     }
 
-    //these represent F_{n-1} and F_{n-2}
-    let mut f_one = 1;
-    let mut f_two: usize;
+    let mut age_counts: VecDeque<BigUint> = VecDeque::with_capacity(m);
+    age_counts.push_front(one());
 
-    for _ in 2..n {
-        f_two = f_one;
-        f_one = count;
-        count += f_two * k;
+    let mut gen_z: BigUint = zero();
+    for _ in 1..n {
+        age_counts.remove(m - 1);
+        age_counts.push_front(gen_z);
+        let mut sum_all = zero();
+        for addend in age_counts.iter() {
+            sum_all += addend;
+        }
+        gen_z = sum_all - age_counts.get(0).unwrap();
     }
 
-    return count;
+    let mut count: BigUint = zero();
+    for addend in age_counts.iter() {
+        count += addend;
+    }
+    count
 }
 
 fn main() {
     let args = Cli::parse();
-    let count = bnuuy_pair_count(args.n, args.k);
+    let count = bnuuy_pair_count(args.n, args.m);
     println!("{}", count);
 }
 
 #[test]
-fn count_5_3() {
-    let (n, k) = (5, 3);
-    let count = bnuuy_pair_count(n, k);
-    assert_eq!(19, count);
+fn count_6_3() {
+    let (n, m) = (6, 3);
+    let count = bnuuy_pair_count(n, m);
+    assert_eq!(4, count);
 }
